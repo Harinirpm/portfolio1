9d124e471586e760451f4f2d1175460d8c2a23b4
Harinirpm <harinirangammal4@gmail.com>
2025-01-09 17:00:34 +0530

    modified home,project

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import './App.css';
 import HorizontalNavbar from './Navbar/HorizontalNavbar';
-// import Home from './Components/Home';
+import Home from './Components/Home';
 import About from './Components/About';
 import Skills from './Components/Skills';
-// import Project from './Components/Project';
+import Project from './Components/Project';
 import Contact from './Components/Contact';
 import SideNavbar from './Navbar/SideNavbar'
 
@@ -16,19 +16,19 @@ function App() {
         <HorizontalNavbar />
         <SideNavbar />
         <div style={{ marginTop: '70px' }}>
-          {/* <div id="#home">
+          <div id="home">
             <Home />
-          </div> */}
-          <div id="#about">
+          </div>
+          <div id="about">
             <About />
           </div>
-          <div id="#skills">
+          <div id="skills">
             <Skills />
           </div>
-          {/* <div id="#projects">
+          <div id="projects">
             <Project />
-          </div> */}
-          <div id="#contact">
+          </div>
+          <div id="contact">
             <Contact />
           </div>
         </div>
--- src/Navbar/HorizontalNavbar.jsx
+++ src/Navbar/HorizontalNavbar.jsx
@@ -7,11 +7,12 @@ function HorizontalNavbar() {
     <nav className='topNav'>
       <h1 className='logo'>Harini's Portfolio</h1>
       <div className='navLinks'>
-        <ScrollLink to="home" smooth={true} duration={500} className='link'>Home</ScrollLink>
-        <ScrollLink to="about" smooth={true} duration={500} className='link'>About</ScrollLink>
-        <ScrollLink to="skills" smooth={true} duration={500} className='link'>Skills</ScrollLink>
-        <ScrollLink to="projects" smooth={true} duration={500} className='link'>Projects</ScrollLink>
-        <ScrollLink to="contact" smooth={true} duration={500} className='link'>Contact</ScrollLink>
+      <ScrollLink to="home" smooth={true} duration={500} offset={-70} className='link'>Home</ScrollLink>
+<ScrollLink to="about" smooth={true} duration={500} offset={-70} className='link'>About</ScrollLink>
+<ScrollLink to="skills" smooth={true} duration={500} offset={-70} className='link'>Skills</ScrollLink>
+<ScrollLink to="projects" smooth={true} duration={500} offset={-70} className='link'>Projects</ScrollLink>
+<ScrollLink to="contact" smooth={true} duration={500} offset={-70} className='link'>Contact</ScrollLink>
+
       </div>
     </nav>
   );
